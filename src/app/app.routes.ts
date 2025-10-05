50da26095d55dab405c05e6bf835decc4bb929e0
ayamedhat229 <ayamedhat773@gmail.com>
2025-10-05 23:36:07 +0300

    dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/components/dashboard/dashboard.component';
+import { UsersComponent } from './dashboard/components/users/users.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/components/dashboard/dashboard.component').then(
+        (m) => m.DashboardComponent
+      ),
+  
+  },
+  {
+    path: 'users',loadComponent: () =>
+      import('./dashboard/components/users/users.component').then(
+        (m) => m.UsersComponent
+      ),
+  }
+
+];
